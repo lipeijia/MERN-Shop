c9c6607ab7d4c0decc41e3ad67af676824620fd0
lipeijia <aoeby13@gmail.com>
2021-10-29 15:03:21 +0800

    img tag add div surrounded

--- components/account/ProductOrder.js
+++ components/account/ProductOrder.js
@@ -3,7 +3,9 @@ export default function ProductOrder({ products }) {
     <>
       {products.map((p, i) => (
         <div key={i} className='flex p-4'>
-          <img src={p.mediaUrl} width='70px' height='70px' />
+          <div>
+            <img src={p.mediaUrl} width='70px' height='70px' />
+          </div>
           <div className='ml-4'>
             <p className='font-serif text-lg'>{p.name}</p>
             <p className='text-gray-500'>{`$${p.price} x ${p.quantity}`}</p>
